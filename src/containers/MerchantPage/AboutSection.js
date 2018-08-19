f9ea6911d72747040d7876d211a8ab0391c22d57
Tamire Workneh <workneh.tamire@dataridoo.com>
2018-08-19 14:10:28 +0300

    mechant page mapsection fixed

--- src/containers/MerchantPage/AboutSection.js
+++ src/containers/MerchantPage/AboutSection.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import css from './MerchantPage.css';
 
 import { FormattedMessage } from 'react-intl';
+import SectionMap from './SectionMap/SectionMap';
 
-import { NamedLink} from '../../components';
 
 const about = ( 
     <div className={css.AboutBody}>    
@@ -13,29 +13,26 @@ const about = (
   </div>  
   
 )
-const searchMap = (
-  <NamedLink name="SearchMapPage">
-    <span className={css.searchMap}>Map</span>
-  </NamedLink>
-)
 
 class AboutSection extends Component{
   render(){
-    return (   
-      <div className="ui stackable grid">
-      <div className="ui eight wide column">
-        <div className={css.AboutSection}>
-         <div className={css.About}> 
-           <p className={css.AboutHeader}>  
-              <FormattedMessage id="MerchantPageAboutSection.AboutHeader" /> 
-           </p>
-             {about}
-         </div>
+    return (  
+      <div className={css.AboutSectionContainer}>
+        <div className="ui stackable grid">
+        <div className="ui eight wide column">
+          <div className={css.AboutSection}>
+          <div className={css.About}> 
+            <p className={css.AboutHeader}>  
+                <FormattedMessage id="MerchantPageAboutSection.AboutHeader" /> 
+            </p>
+              {about}
+          </div>
+          </div>
         </div>
-      </div>
-    <div className="ui eight wide column">
-      <div className={css.MapSection}>
-       {searchMap}
+      <div className="ui eight wide column">
+        <div className={css.MapSection}>
+        <SectionMap />
+        </div>
       </div>
     </div>
   </div>
